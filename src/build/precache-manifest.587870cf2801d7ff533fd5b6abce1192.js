8deeaf6dfab9ab116a5b42fd8a855c5175b05b2d
balimysore <mysoreb@expovision.com>
2020-08-28 08:15:30 -0400

    first commit

---
+++ src/build/precache-manifest.587870cf2801d7ff533fd5b6abce1192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78068fe95cd77859367a294ee41d94db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7b4a4061576e1229e2e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84b5d7e10c9c06ca67a0",
+    "url": "/static/js/2.40d6246e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40d6246e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b4a4061576e1229e2e",
+    "url": "/static/js/main.b50ae242.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
